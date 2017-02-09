a5ee54f63ab5852d6530e3d2b00c068b64e34cf9
yd8266uj <yd8266uj@metrostate.edu>
2017-02-09 02:19:25 -0600

    /* comments */

--- src/edu/metrostate/ics372/tigersharks/FileReader.java
+++ src/edu/metrostate/ics372/tigersharks/FileReader.java
@@ -10,11 +10,21 @@ import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
 /**
- * Created by sleig on 1/29/2017.
+ * Responsible for generating a supply of Loanable items from a JSON file.
  */
 public class FileReader implements Supplier<Loanable> {
+    /**
+     * A list of items read from the file reader.
+     */
     private final LinkedList<JSONObject> data = new LinkedList<>();
 
+    /**
+     * Read contents of a file into data List.
+     *
+     * Parse the file contents. Retrieve "library_items" key as List<JSONObject>. Add each element of List to data.
+     *
+     * @param file initialized with path to json file.
+     */
     public FileReader(java.io.FileReader file) {
         try {
             data.addAll((JSONArray)((JSONObject) new JSONParser().parse( file )).get("library_items"));
@@ -23,19 +33,40 @@ public class FileReader implements Supplier<Loanable> {
         }
     }
 
+    /**
+     * Provide the first Loanable
+     *
+     * Retrieve the first item of data and use it to create a new loanable.
+     *
+     * @return first Loanable item
+     */
     @Override
     public Loanable get() {
         return new Map().apply(data.pollFirst());
     }
 
+    /**
+     * provide the length of data
+     *
+     * @return the length of data
+     */
     public int size() {
         return data.size();
     }
 
+    /**
+     * Responsible for converting parsed JSONObjects to a Loanable.
+     */
     private static class Map implements Function<JSONObject, Loanable> {
+        /**
+         * A mapping function. converts a JSONObject to a Loanable.
+         *
+         * @param o JSONOBject as HashMAp with data to initialize new Loanable.
+         * @return Loanable instance of LibraryItem.
+         */
         @Override
         public Loanable apply(JSONObject o) {
-            if(!o.containsKey("item_key")) return null;
+            if(!o.containsKey("item_type")) return null;
             switch (o.get("item_type").toString().toLowerCase()) {
                 case "cd":
                     return LibraryItem.makeLibraryItem(o.get("item_name").toString(), o.get("item_id").toString(), LibraryItem.Type.CD, o.get("item_artist").toString());
--- src/edu/metrostate/ics372/tigersharks/Loanable.java
+++ src/edu/metrostate/ics372/tigersharks/Loanable.java
@@ -6,7 +6,26 @@ import java.time.LocalDate;
  * Created by sleig on 1/29/2017.
  */
 public interface Loanable {
+    /**
+     * Try to check out Loanable
+     *
+     * Calculate due date. Update Loanable state to checked out.
+     *
+     * @return due date
+     */
     LocalDate checkout();
+
+    /**
+     * Update Loanable state to checked in.
+     *
+     * @return success value
+     */
     Boolean checkin();
+
+    /**
+     * Getter itemId
+     *
+     * @return item identifier
+     */
     String getItemId();
 }
