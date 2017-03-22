0288194f49f4018bb4e0d0ee74eb18208abb53c0
yd8266uj <yd8266uj@metrostate.edu>
2017-03-22 07:25:28 -0500

    added a Main class so its more obvious how to run

---
+++ src/edu/metrostate/ics372/tigersharks/Main.java
@@ -0,0 +1,12 @@
+package edu.metrostate.ics372.tigersharks;
+
+import edu.metrostate.ics372.tigersharks.www.WebService;
+
+/**
+ * Created by sleig on 3/22/2017.
+ */
+public class Main {
+    public static void main(String[] args) {
+        WebService.main(args);
+    }
+}
